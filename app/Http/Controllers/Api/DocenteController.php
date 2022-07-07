a1c78da5c54ba0a878d6eb11d10068929cb218e8
MARIOENRIQUE\tenka <mariomartinez.isc@gmail.com>
2022-07-07 09:49:18 -0500

    Se agrego import storage

--- app/Http/Controllers/Api/DocenteController.php
+++ app/Http/Controllers/Api/DocenteController.php
@@ -12,6 +12,7 @@ use App\Models\C_docenteFormacion;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 
 class DocenteController extends Controller
 {
